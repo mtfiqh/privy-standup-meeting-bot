0a03fca775cd2ef751f02efb420f37d8c8131adc
Jose Sitanggang <jose.stnggng@gmail.com>
2019-07-25 12:35:42 +0700

    update advice

--- app/advice.js
+++ app/advice.js
@@ -1,25 +1,26 @@
 
-class Advice  {
+const {App} = require('../core/App')
+
+class Advice  extends App {
 
     constructor(prefix, id, name){
-
+        super()
         this.prefix = prefix;
         this.id = id
         this.name = name
+
+        this.register([
+            'onRequest'
+        ])
     }
 
     onRequest(){
         return {
+            type:"Listen",
+            id: this.id,
             message: 'Masukkan Masukan Anda!',
             options: {
-                parse_mode: "Markdown",
-                reply_markup:{
-                    keyboard:[
-                        ['Save'],
-                        ["Cancel"]
-                    ],
-                    resize_keyboard: true
-                }
+                parse_mode: "Markdown"
             }
         }
     }
